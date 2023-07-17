e1803f84334bea7e2f2464d093d8a6573bf7394a
unknown <ayoubnasraoui161@yahoo.fr>
2023-07-17 12:07:22 +0200

    Adding environement variable

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require("dotenv").config()
 const express=require("express");
 const ejs=require("ejs");
 const bodyParser=require("body-parser");
@@ -15,8 +16,8 @@ const userSchema=new mongoose.Schema({
         required:true
     }
 });
-const secret="thisisagoodsecrettokeep";
-userSchema.plugin(encrypt, { secret: secret,encryptedFields: ['password'] });
+
+userSchema.plugin(encrypt, { secret:process.env.SECRET,encryptedFields: ['password'] });
 const User=mongoose.model("user",userSchema);
 
 async function connectDB(url){
